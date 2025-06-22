b2ecaf68a917ea2a5619cca085e6942cc885d969
M.Farouq Abdillah <61816268+abdilfaruq@users.noreply.github.com>
2025-06-23 02:15:33 +0700

    fix vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://sales-pulse-production-2278.up.railway.app',
         changeOrigin: true,
         rewrite: (path) => path,
       },
