061287fc5ba75f9eb4b43214987c0749e51cc21e
M.Farouq Abdillah <61816268+abdilfaruq@users.noreply.github.com>
2025-06-23 02:00:34 +0700

    fix knexfile.js

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,26 +3,32 @@ require('dotenv').config();
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
-      host: process.env.PG_HOST || 'localhost',
-      port: process.env.PG_PORT || 5432,
-      user: process.env.PG_USER || 'postgres',
-      password: process.env.PG_PASSWORD || '123456',
-      database: process.env.PG_DATABASE || 'sales_pulse'
-    },
+    connection: process.env.DATABASE_URL && process.env.DATABASE_URL.trim() !== ''
+    ? process.env.DATABASE_URL
+    : {
+        host: process.env.PG_HOST || 'localhost',
+        port: process.env.PG_PORT || 5432,
+        user: process.env.PG_USER || 'postgres',
+        password: process.env.PG_PASSWORD || '123456',
+        database: process.env.PG_DATABASE || 'sales_pulse'
+      },
     migrations: {
       directory: './migrations'
     }
   },
   production: {
     client: 'pg',
-    connection: {
-      host: process.env.PG_HOST,
-      port: process.env.PG_PORT,
-      user: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE
-    },
+    connection: process.env.DATABASE_URL && process.env.DATABASE_URL.trim() !== ''
+      ? {
+          connectionString: process.env.DATABASE_URL,
+        }
+      : {
+          host: process.env.PG_HOST,
+          port: process.env.PG_PORT,
+          user: process.env.PG_USER,
+          password: process.env.PG_PASSWORD,
+          database: process.env.PG_DATABASE
+        },
     migrations: {
       directory: './migrations'
     },
